2144b238603277d384310b60390d899f559ca098
Praveen <Praveenprasad2510@gmail.com>
2023-06-06 18:13:45 +0530

    Completed signup page

--- Notebook/src/Components/Signup/Signup.jsx
+++ Notebook/src/Components/Signup/Signup.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import "./signup.scss"
 
 function Signup() {
   return (
     <div className="signup-wrapper">
+        <h1 className="signup-heading">Signup</h1>
         <div className="signup-inputs">
         <TextField id="standard-basic" label="Email" variant="standard" type='email' /><br />
-        <TextField id="standard-basic" label="password" variant="standard" type='password' /><br /><br />
+        <TextField id="standard-basic" label="Password" variant="standard" type='password' /><br /><br />
         </div>
         <div className="signup-buttons">
-        <Button variant="outlined">Signup</Button>
+        <Button variant="contained">Signup</Button>
         </div>
     </div>
   )
