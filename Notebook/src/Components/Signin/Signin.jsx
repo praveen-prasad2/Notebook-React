eb52f7ba5d10347d4a7ec49868a6a3b57c904d9a
Praveen <Praveenprasad2510@gmail.com>
2023-06-07 15:11:50 +0530

    completed signup and signin

--- Notebook/src/Components/Signin/Signin.jsx
+++ Notebook/src/Components/Signin/Signin.jsx
@@ -2,19 +2,39 @@ import React from 'react'
 import { Button, TextField } from '@mui/material'
 import "./signin.scss"
 import Navbar from '../Navbar/Navbar'
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Signin() {
+
+const emailRef=useRef()
+const passRef=useRef()
+
+const navigate=useNavigate()
+
+  const localEmail=localStorage.getItem("email")
+  const localPassword=localStorage.getItem("password")
+
+  async function userSignin(){
+
+    if(emailRef.current.value==localEmail && passRef.current.value==localPassword){
+        navigate("/addnote")
+    }else{
+      alert("check login details")
+    }
+  }
+
   return (
 <>
     <Navbar />
     <div className="signin-wrapper">
     <h1 className="signin-heading">Signin</h1>
     <div className="signin-inputs">
-    <TextField id="standard-basic" label="Email" variant="standard" type='email' /><br />
-    <TextField id="standard-basic" label="Password" variant="standard" type='password' /><br /><br />
+    <TextField id="standard-basic" label="Email" variant="standard" type='email' inputRef={emailRef}/><br />
+    <TextField id="standard-basic" label="Password" variant="standard" type='password' inputRef={passRef} /><br /><br />
     </div>
     <div className="signin-buttons">
-    <Button variant="contained">Signin</Button>
+    <Button variant="contained" onClick={userSignin}>Signin</Button>
     </div>
 </div>
 </>
--- Notebook/src/Components/Signup/Signup.jsx
+++ Notebook/src/Components/Signup/Signup.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Navbar from "../Navbar/Navbar"
 import "./signup.scss"
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
+
+  const [fullName,setFullname]=useState()
+  const [email,setEmail]=useState()
+  const [password,setPassword]=useState()
+
+  const[signupedUser,setSignupedUSer]=useState()
+
+  const navigate=useNavigate()
+
+  async function userSignup(){
+
+    if(fullName && email && password){
+      localStorage.setItem("fullname",fullName)
+      localStorage.setItem("email",email)
+      localStorage.setItem("password",password)
+      localStorage.setItem("signup",email)
+
+      alert("Account created successfully")
+      navigate("/signin")
+    }
+    // console.log(fullName+email+password);
+  
+  }
   return (
     <>
     <Navbar/>
     <div className="signup-wrapper">
         <h1 className="signup-heading">Signup</h1>
         <div className="signup-inputs">
-        <TextField id="standard-basic" label="Fullname" variant="standard" type='text' /><br />
-        <TextField id="standard-basic" label="Email" variant="standard" type='email' /><br />
-        <TextField id="standard-basic" label="Password" variant="standard" type='password' /><br /><br />
+        <TextField id="standard-basic" label="Fullname" variant="standard" type='text' onChange={(f) => setFullname(f.target.value)} /><br />
+        <TextField id="standard-basic" label="Email" variant="standard" type='email' onChange={(e) => setEmail(e.target.value)} /><br />
+        <TextField id="standard-basic" label="Password" variant="standard" type='password' onChange={(p) => setPassword(p.target.value)} /><br /><br />
         </div>
         <div className="signup-buttons">
-        <Button variant="contained">Signup</Button>
+        <Button variant="contained" onClick={userSignup}>Signup</Button>
         </div>
     </div>
     </>
